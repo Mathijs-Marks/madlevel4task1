d665e35ea2671dbde44259907d96262df1edc651
Mathijs <mathijsmarks@msn.com>
2021-03-21 22:44:54 +0100

    Created the User Interface, Adapter, and data class.

---
+++ app/src/main/java/com/example/madlevel4task1/Product.kt
@@ -0,0 +1,5 @@
+package com.example.madlevel4task1
+
+data class Product (
+    var productText: String
+        )
---
+++ app/src/main/java/com/example/madlevel4task1/ShoppingListAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.madlevel4task1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4task1.databinding.ItemProductBinding
+
+class ShoppingListAdapter(private val products: List<Product>) : RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val binding = ItemProductBinding.bind(itemView)
+
+        fun dataBind(product: Product) {
+            binding.tvProduct.text = product.productText
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.dataBind(products[position])
+    }
+}
