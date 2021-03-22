22e7b8ec097e5688bf6dffae1ac11d3023f747f5
Mathijs <mathijsmarks@msn.com>
2021-03-22 12:11:48 +0100

    Configured Room database.

--- app/src/main/java/com/example/madlevel4task1/model/Product.kt
+++ app/src/main/java/com/example/madlevel4task1/model/Product.kt
@@ -1,5 +1,18 @@
 package com.example.madlevel4task1.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "productTable")
 data class Product (
-    var productText: String
+
+        @ColumnInfo(name = "product")
+        var productQuantity: Int,
+        var productText: String,
+
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Long? = null
+
         )
